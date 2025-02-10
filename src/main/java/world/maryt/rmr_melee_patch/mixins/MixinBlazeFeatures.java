18b7775ed7ae34625f7f1de3029b0d82c10ae20f
RisingInIris2017 <risinginiris2017@gmail.com>
2025-02-10 21:03:04 +0800

    v1.0.0

--- src/main/java/world/maryt/rmr_melee_patch/mixins/MixinBlazeFeatures.java
+++ src/main/java/world/maryt/rmr_melee_patch/mixins/MixinBlazeFeatures.java
@@ -1,7 +1,15 @@
 package world.maryt.rmr_melee_patch.mixins;
 import com.p1ut0nium.roughmobsrevamped.config.RoughConfig;
 import com.p1ut0nium.roughmobsrevamped.features.BlazeFeatures;
+import com.p1ut0nium.roughmobsrevamped.misc.FeatureHelper;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.init.SoundEvents;
+import net.minecraft.util.DamageSource;
+import net.minecraftforge.event.entity.living.LivingAttackEvent;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -10,8 +18,21 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value = BlazeFeatures.class, remap = false)
 public abstract class MixinBlazeFeatures {
+
+    @Shadow
+    private boolean pushAttackersAway;
+    @Shadow
+    private boolean flameTouch;
+    @Shadow
+    private float pushStrength;
+
     @Unique
-    float roughMobsRevamped_MeleePatch$blazeFeatureRange;
+    float rMR_MeleePatch$blazeFeatureRange;
+
+    @Unique
+    boolean rMR_MeleePatch$isWithinFeatureRange(Entity entityA, Entity entityB) {
+        return (rMR_MeleePatch$blazeFeatureRange > 0) && entityA.getDistance(entityB) <= rMR_MeleePatch$blazeFeatureRange;
+    }
 
     @Inject(
             method = "initConfig",
@@ -23,12 +44,31 @@ public abstract class MixinBlazeFeatures {
             )
     )
     private void inject_initConfig(CallbackInfo ci) {
-        roughMobsRevamped_MeleePatch$blazeFeatureRange = RoughConfig.getFloat(
+        rMR_MeleePatch$blazeFeatureRange = RoughConfig.getFloat(
                 ((BlazeFeatures)(Object)this).name,
-                "BlazeFeatureRange",
+                "FeatureRange",
                 1.0f,
                 -1.0f,
                 16.0f,
                 "Blaze only affect targets within this radius with its features. Value not greater than 0 disables range check.");
+    }
+
+    /**
+     * @author RisingInIris2017
+     * @reason add radius check
+     */
+    @Overwrite
+    public void onDefend(Entity target, Entity attacker, Entity immediateAttacker, LivingAttackEvent event) {
+        if (this.pushAttackersAway &&
+                attacker instanceof EntityLivingBase &&
+                attacker == immediateAttacker && rMR_MeleePatch$isWithinFeatureRange(target, immediateAttacker)) {
+            FeatureHelper.knockback(target, (EntityLivingBase)attacker, 1.0F, 0.05F);
+            attacker.attackEntityFrom(DamageSource.GENERIC, this.pushStrength);
+            if (this.flameTouch) {
+                attacker.setFire(8);
+            }
+
+            FeatureHelper.playSound(target, SoundEvents.BLOCK_FIRE_EXTINGUISH, 0.7F, 1.0F);
+        }
     }
 }
